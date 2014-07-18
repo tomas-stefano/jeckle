13beec51cf74d1987ea7bac59d03c18f301757e0
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-07-18 12:45:56 +0200

    Add httparty request

--- lib/jeckle.rb
+++ lib/jeckle.rb
@@ -3,6 +3,7 @@ require 'active_support/dependencies/autoload'
 require 'active_support/concern'
 require 'virtus'
 require 'active_model'
+require 'httparty'
 
 module Jeckle
   extend ActiveSupport::Autoload
