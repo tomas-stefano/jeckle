0a9f4e0328b99fbb4debfc88f5232c1e0bc146d4
Brenno Costa <brennolncosta@gmail.com>
2014-08-14 01:13:01 -0300

    Improve spec setup by moving Jeckle::Setup to be used globally

--- spec/jeckle/setup_spec.rb
+++ spec/jeckle/setup_spec.rb
@@ -5,24 +5,13 @@ RSpec.describe Jeckle::Setup do
     context 'when block is given' do
       let(:registered_apis) { Jeckle::Setup.registered_apis }
 
-      before :all do
-        Jeckle::Setup.register(:my_super_api) do |api|
-          api.base_uri = 'http://my_super.api.com.br'
-          api.headers = { 'Content-Type' => 'application/json' }
-          api.logger = Logger.new(STDOUT)
-          api.basic_auth = { username: 'steven_seagal', password: 'youAlwaysLose' }
-          api.namespaces = { version: 'v1' }
-          api.params = { hello: 'world' }
-        end
-      end
-
       it 'returns a new registered API' do
         expect(registered_apis).to have_key(:my_super_api)
       end
 
       describe 'base uri' do
         it 'assigns to the api instance' do
-          expect(registered_apis[:my_super_api].base_uri).to eq 'http://my_super.api.com.br'
+          expect(registered_apis[:my_super_api].base_uri).to eq 'http://my-super-api.com.br'
         end
       end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,15 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
 
   config.before :suite do
+    Jeckle::Setup.register(:my_super_api) do |api|
+      api.base_uri = 'http://my-super-api.com.br'
+      api.headers = { 'Content-Type' => 'application/json' }
+      api.logger = Logger.new(STDOUT)
+      api.basic_auth = { username: 'steven_seagal', password: 'youAlwaysLose' }
+      api.namespaces = { version: 'v1' }
+      api.params = { hello: 'world' }
+    end
+
     class FakeResource
       include Jeckle::Resource
     end
