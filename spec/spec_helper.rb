0eb88ea3de34d90052d59f627d0fe85a49e08a36
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-09-09 13:33:16 +0200

    Organize the specs because in the future this gem will grow and have more examples to test against

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,32 +1,10 @@
 require 'bundler/setup'
-
-Dir['spec/support/**/*.rb'].each { |file| require File.expand_path(file) }
-
 require 'jeckle'
+
+%w(support fixtures).each do |dir|
+  Dir["spec/#{dir}/**/*.rb"].each { |file| require File.expand_path(file) }
+end
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
-
-  config.before :suite do
-    Jeckle::Setup.register(:my_super_api) do |api|
-      api.base_uri = 'http://my-super-api.com.br'
-      api.headers = { 'Content-Type' => 'application/json' }
-      api.logger = Logger.new(STDOUT)
-      api.basic_auth = { username: 'steven_seagal', password: 'youAlwaysLose' }
-      api.namespaces = { prefix: 'api', version: 'v1' }
-      api.params = { hello: 'world' }
-    end
-
-    class FakeModel
-      include Jeckle::Model
-    end
-
-    class FakeResource
-      include Jeckle::Resource
-
-      default_api :my_super_api
-
-      attribute :id, Integer
-    end
-  end
 end
