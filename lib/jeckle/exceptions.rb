b62a23db97286e4318152ef08791ccca4b52e5aa
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-09-09 20:22:01 +0200

    Change variable to become more readable. File to file name

--- lib/jeckle/exceptions.rb
+++ lib/jeckle/exceptions.rb
@@ -1,4 +1,4 @@
 
-%w(no_such_api_error no_username_or_password_error).each do |file|
-  require "jeckle/exceptions/#{file}"
+%w(no_such_api_error no_username_or_password_error).each do |file_name|
+  require "jeckle/exceptions/#{file_name}"
 end
