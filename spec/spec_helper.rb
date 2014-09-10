20052737eb6bb39f28dc2aeb91eee148cc24aa9b
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-09-10 19:25:49 +0200

    Force the load from jeckle config fixture before loading the fixtures directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 require 'jeckle'
+require 'fixtures/jeckle_config'
 
 %w(support fixtures).each do |dir|
   Dir["spec/#{dir}/**/*.rb"].each { |file| require File.expand_path(file) }
