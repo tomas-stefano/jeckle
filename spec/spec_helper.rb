5758e676c5ccccd89c16eb753cfc1b02ab899581
Brenno Costa <brennolncosta@gmail.com>
2014-10-08 01:07:32 -0300

    Code Climate coverage setup should be at the very top of the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require 'bundler/setup'
 require 'jeckle'
 
-Dir["spec/support/**/*.rb"].each { |file| require File.expand_path(file) }
+Dir['spec/support/**/*.rb'].each { |file| require File.expand_path(file) }
 
 require 'fixtures/jeckle_config'
 
-Dir["spec/fixtures/**/*.rb"].each { |file| require File.expand_path(file) }
+Dir['spec/fixtures/**/*.rb'].each { |file| require File.expand_path(file) }
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
