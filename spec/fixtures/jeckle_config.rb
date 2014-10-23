fbcd77ebcc0ab4edb3d1e427f5c4975493b17014
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-10-23 03:50:29 +0100

    Log all Jeckle Responses in debug mode.
    * This change are so helpful when you're debuging the API responses in dev mode.

--- spec/fixtures/jeckle_config.rb
+++ spec/fixtures/jeckle_config.rb
@@ -2,7 +2,7 @@ Jeckle.configure do |config|
   config.register :my_super_api do |api|
     api.base_uri = 'http://my-super-api.com.br'
     api.headers = { 'Content-Type' => 'application/json' }
-    api.logger = Logger.new(STDOUT)
+    api.logger = Logger.new('spec/requests.log')
     api.basic_auth = { username: 'steven_seagal', password: 'youAlwaysLose' }
     api.namespaces = { prefix: 'api', version: 'v1' }
     api.params = { hello: 'world' }
