1e57e52cbd054b2998e1c125c7141ebcb5f1c09c
Mateus Gomes <mateusg.18@gmail.com>
2015-05-10 20:11:09 -0300

    Separate params into another hash to avoid conflict with options (method/body/headers) [solves #52]

--- spec/lib/jeckle/rest_actions_spec.rb
+++ spec/lib/jeckle/rest_actions_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Jeckle::RESTActions do
 
       it 'calls default API connection with GET and search params' do
         expect(Jeckle::Request).to receive(:run)
-          .with(api, 'fake_resources', query).and_return(fake_request)
+          .with(api, 'fake_resources', params: query).and_return(fake_request)
 
         FakeResource.search query
       end
