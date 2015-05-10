36c1db701ae9a2cddd1a8a90704f417754c561e7
Mateus Gomes <mateusg.18@gmail.com>
2015-05-10 19:07:52 -0300

    Add RSpec random config and format as doc when 1 file in run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,4 +15,16 @@ Dir['spec/fixtures/**/*.rb'].each { |file| require File.expand_path(file) }
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
 end
